a4dff69f790d2560d41d64c5a113ebc0e5d8c208
Jiye You <mine@Jiyes-MacBook-Pro.local>
2023-11-10 19:45:45 -0700

    change error

--- iatsoundextension.js
+++ iatsoundextension.js
@@ -622,7 +622,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			}],
 			// this stimulus used for giving feedback, in this case only the error notification
 			error : [{
-				handle:'error', location: {top: 75}, css:{color:'red','font-size':'4em'}, media: {word:'X'}, nolog:true
+				handle:'error', css:{color:'red'}, media: {word:'error'}, nolog:true
 			}],
 
 			touchInputStimuli: [
