3092ff080713b7e0b1cb8b773da609f2f4916ff0
Jiye You <mine@Jiyes-MacBook-Pro.local>
2023-11-11 01:09:07 -0700

    change of audio stimuli

--- QualtricsIAT.js
+++ QualtricsIAT.js
@@ -5,85 +5,77 @@ define(['pipAPI','https://youjenjen.github.io/iat/iatsoundextension.js'], functi
   //Randomly choose the attribute words from a larger list of attribute words.
   //Internal studies in Project Implicit found no difference in performance between the words.
   var posWords = API.shuffle([
-	'loose','open','airy','spacious','uncrowded','roomy','sparse','scattered']);
+	'spacious','uncrowded','roomy','sparse','scattered']);
   var negWords = API.shuffle([
-	'crowded','thick','packed','tight','compact','crammed','condensed','close-knit']);
+	'packed','tight','compact','crammed','condensed']);
   
     return iatExtension({
         category1 : {
-            name : 'low pitch', //Will appear in the data.
+            name : 'dense', //Will appear in the data.
             title : {
-                media : {word : 'low pitch'}, //Name of the category presented in the task.
+                media : {word : 'dense'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: '...',sound: soundUrl + 'sound1.mp3'},
-                {word: '...',sound: soundUrl + 'sound2.mp3'},
-                {word: '...',sound: soundUrl + 'sound3.mp3'},
-				{image: 'bf1_nc.jpg'},
-                {image: 'bf2_nc.jpg'},                 
-				{image: 'bf3_nc.jpg'}     
+				{word: negWords[0]},
+				{word: negWords[1]},
+				{word: negWords[2]},
+				{word: negWords[3]},
+				{word: negWords[4]},  
     	    ],
     		//Stimulus css (style)
     		stimulusCss : {color:'#31940F','font-size':'2.3em'}
         },    
         category2 :    {
-            name : 'high pitch', //Will appear in the data.
+            name : 'not dense', //Will appear in the data.
             title : {
-                media : {word : 'high pitch'}, //Name of the category presented in the task.
+                media : {word : 'not dense'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'wm1_nc.jpg'},
-                {image: 'wm2_nc.jpg'},
-                {image: 'wm3_nc.jpg'},
-				{image: 'wf1_nc.jpg'},
-            	{image: 'wf2_nc.jpg'},
-            	{image: 'wf3_nc.jpg'}
+				{word: posWords[0]},
+				{word: posWords[1]},
+				{word: posWords[2]},
+				{word: posWords[3]},
+				{word: posWords[4]},
             ],
     		//Stimulus css (style)
     		stimulusCss : {color:'#31940F','font-size':'2.3em'}
         },
 		attribute1 :
 		{
-			name : 'dense',
+			name : 'low pitch',
 			title : {
-				media : {word : 'dense'},
+				media : {word : 'low pitch'},
 				css : {color:'#0000FF','font-size':'1.8em'},
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-				{word: negWords[0]},
-				{word: negWords[1]},
-				{word: negWords[2]},
-				{word: negWords[3]},
-				{word: negWords[4]},
-				{word: negWords[5]},
-				{word: negWords[6]},
-				{word: negWords[7]}
+				{word: '...',sound: soundUrl + 'Bass.arco.ff.sulE.D2.stereo.aif'},
+				{word: '...',sound: soundUrl + 'Bassoon.ff.D2.stereo.aif'},
+				{word: '...',sound: soundUrl + 'Horn.ff.D2.stereo.aif'},
+				{word: '...',sound: soundUrl + 'Piano.ff.D2.aif'},
+				{word: '...',sound: soundUrl + 'Tuba.ff.D2.stereo.aif'}
 			],
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 		},
 		attribute2 :
 		{
-			name : 'not dense',
+			name : 'high pitch',
 			title : {
-				media : {word : 'not dense'},
+				media : {word : 'high pitch'},
 				css : {color:'#0000FF','font-size':'1.8em'},
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-				{word: posWords[0]},
-				{word: posWords[1]},
-				{word: posWords[2]},
-				{word: posWords[3]},
-				{word: posWords[4]},
-				{word: posWords[5]},
-				{word: posWords[6]},
-				{word: posWords[7]}
+				{word: '...',sound: soundUrl + 'BbClarinet.ff.C6.stereo.aif'},
+				{word: '...',sound: soundUrl + 'Flute.nonvib.ff.C6.stereo.aif'},
+				{word: '...',sound: soundUrl + 'Piano.ff.C6.aif'},
+				{word: '...',sound: soundUrl + 'Trumpet.novib.ff.C6.stereo.aif'},
+				{word: '...',sound: soundUrl + 'Viola.arco.ff.sulA.C6.stereo.aif'}
 			],
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
