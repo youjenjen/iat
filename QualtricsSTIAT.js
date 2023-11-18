f97e024b9974e74f07fa93a172f52022eb4a0501
Jiye You <mine@Jiyes-MacBook-Pro.local>
2023-11-18 13:29:31 -0700

    back to original

--- QualtricsSTIAT.js
+++ QualtricsSTIAT.js
@@ -1,5 +1,6 @@
-define(['pipAPI', 'https://youjenjen.github.io/iat/stiatFramework.js'], function(APIConstructor, stiatExtension){
-
+define(['pipAPI', 'https://youjenjen.github.io/iat/original_stiatFramework.js'], function(APIConstructor, stiatExtension){
+	var soundUrl = 'https://youjenjen.github.io/iat/images/';
+  
 	var API = new APIConstructor();
 		  return stiatExtension({
 
