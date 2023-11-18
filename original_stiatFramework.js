9387453ef471ea62efa00209275f2ade79a4dc01
Jiye You <mine@Jiyes-MacBook-Pro.local>
2023-11-18 14:49:47 -0700

    original

--- original_stiatFramework.js
+++ original_stiatFramework.js
@@ -327,47 +327,6 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
             }
         });
 
-		/**
-		 * setup sound support
-		 **/
-		var Sound = (function(){
-			var soundCache = {}; // url: Audio
-			var currentTrial;
-
-			// preload sounds
-			_([att1,att2,cat1,cat2])
-				.map(function(cat){return cat.media;})
-				.flatten()
-				.forEach(function(media){
-					if (media && media.sound){
-						soundCache[media.sound] = new Audio(media.sound);
-					}
-				});
-				//.value();
-				
-			return {
-				start: function(){
-					var src = _.get(currentTrial, 'stimuli[0].$templated.media.$templated.sound');
-				    console.log(src);
-					if (src){
-						soundCache[src].play();
-					}
-				},
-				
-				stop: function(){
-					var src = _.get(currentTrial, 'stimuli[0].$templated.media.$templated.sound');
-					if (src){
-						soundCache[src].pause();
-						soundCache[src].currentTime = 0;
-					}
-				},
-				
-				setup: function(trialSource){
-					currentTrial = trialSource; // we can't use it at this point in time. We need to wait until stim/media are poplated.	
-				}
-			}
-		})();
-
 		/***********************************************************************************
 		*
 		* Here starts the script. You might not need to change anything in the actual script.
@@ -425,15 +384,12 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				{handle:'right',on:'keypressed',key:'i'}
 			],
 
-			customize: Sound.setup,
-
 			// user interactions
 			interactions: [
 				// begin trial : display stimulus immediately
 				{
 					conditions: [{type:'begin'}],
-					actions: [{type:'showStim',handle:'targetStim'},
-					{type:'custom', fn: Sound.start}]
+					actions: [{type:'showStim',handle:'targetStim'}]
 				},
 
 				// error
@@ -454,7 +410,6 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 					actions: [
 						{type:'removeInput',handle:['left','right']}, //Cannot respond anymore
 						{type:'hideStim', handle: 'All'},											// hide everything
-						{type:'custom', fn: Sound.stop},
 						{type:'log'},																// log this trial
 						{type:'setInput',input:{handle:'end', on:'timeout',duration:piCurrent.ITIDuration}} // trigger the "end action after ITI"
 					]
