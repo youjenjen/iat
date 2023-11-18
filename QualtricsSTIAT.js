28d1e7e863c31489396e2cecfc002ed8fc6deee4
Jiye You <mine@Jiyes-MacBook-Pro.local>
2023-11-18 14:42:34 -0700

    only sound

--- QualtricsSTIAT.js
+++ QualtricsSTIAT.js
@@ -33,12 +33,11 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 				height : 7 //Used to position the "Or" in the combined block.
 			}, 
 			media : [ //Stimuli
-				{word: 'Bomb'},
-				{word: 'Abuse'},
-				{word: 'Sadness'},
-				{word: 'Pain'},
-				{word: 'Poison'},
-				{word: 'Grief'}
+				{word: '...',sound: soundUrl + 'adj_Bass.arco.ff.sulE.D2.stereo.wav'},
+				{word: '...',sound: soundUrl + 'adj_Bassoon.ff.D2.stereo.wav'},
+				{word: '...',sound: soundUrl + 'adj_Horn.ff.D2.stereo.wav'},
+				{word: '...',sound: soundUrl + 'adj_Piano.ff.D2.wav'},
+				{word: '...',sound: soundUrl + 'adj_Tuba.ff.D2.stereo.wav'}
 			], 
 			//Can change color and size of the targets here.
 			css : {color:'#31b404','font-size':'3em'}
